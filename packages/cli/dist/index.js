10a6069d142a269200ded06b7c955ff05331226a
Terry Nguyen <terryknguyen@gmail.com>
2026-06-03 17:10:22 -0400

    Edit information

--- packages/cli/dist/index.js
+++ packages/cli/dist/index.js
@@ -2,18 +2,30 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const commander_1 = require("commander");
+const child_process_1 = require("child_process");
 const program = new commander_1.Command();
 program
     .name("native-blocks")
+    .description("Mobile-first React Native component registry")
     .version("0.1.0");
 program
     .command("init")
+    .description("Initialize Native Blocks in your project")
     .action(() => {
-    console.log("Initializing Native Blocks");
+    console.log("🚀 Initializing Native Blocks...");
+    (0, child_process_1.spawnSync)("npx", ["@react-native-reusables/cli@latest", "init"], {
+        stdio: "inherit",
+        shell: true,
+    });
 });
 program
     .command("add <component>")
+    .description("Add a component to your project")
     .action((component) => {
-    console.log(`Installing ${component}`);
+    console.log(`📦 Adding ${component} to your project...`);
+    (0, child_process_1.spawnSync)("npx", ["@react-native-reusables/cli@latest", "add", component], {
+        stdio: "inherit",
+        shell: true,
+    });
 });
 program.parse();
--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -1,23 +1,35 @@
 #!/usr/bin/env node
 
 import { Command } from "commander";
+import { spawnSync } from "child_process";
 
 const program = new Command();
 
 program
     .name("native-blocks")
+    .description("Mobile-first React Native component registry")
     .version("0.1.0");
 
 program
     .command("init")
+    .description("Initialize Native Blocks in your project")
     .action(() => {
-        console.log("Initializing Native Blocks");
+        console.log("🚀 Initializing Native Blocks...");
+        spawnSync("npx", ["@react-native-reusables/cli@latest", "init"], {
+            stdio: "inherit",
+            shell: true,
+        });
     });
 
 program
     .command("add <component>")
+    .description("Add a component to your project")
     .action((component) => {
-        console.log(`Installing ${component}`);
+        console.log(`📦 Adding ${component} to your project...`);
+        spawnSync("npx", ["@react-native-reusables/cli@latest", "add", component], {
+            stdio: "inherit",
+            shell: true,
+        });
     });
 
 program.parse();
